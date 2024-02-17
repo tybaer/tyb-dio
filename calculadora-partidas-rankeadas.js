3c0927204143f59017e15731f42712d1fd72987d
tybaer <tahneebaer@hotmail.com>
2024-02-17 00:13:40 -0300

    calculadora-partidas-rankeadas-finalizado

--- calculadora-partidas-rankeadas.js
+++ calculadora-partidas-rankeadas.js
@@ -1,38 +1,44 @@
-let vitorias = " "
-let derrotas = " "
+
+
+
+let vitorias = 40
+let derrotas = 10
+let resultado = calcularNivel(vitorias,derrotas)
 
 //Calculo da Partida
 function calcularNivel(vitorias, derrotas){
-    saldo = vitorias - derrotas
+    let saldo = vitorias - derrotas
     return saldo
-
-    console.log("O Herói tem saldo " + saldo + "e está no nível de " + nivel)
 }
 
 //Nivel do jogador
-function nivelJogador 
-    
-    if vitorias < 10:
-        nivel = "Ferro"
-    elif 11 <= vitorias <= 20:
-        nivel = "Bronze"
-    elif 21 <= vitorias <= 50:
-        nivel = "Prata"
-    elif 51 <= vitorias <= 80:
-        nivel = "Ouro"
-    elif 81 <= vitorias <= 90:
-        nivel = "Diamante"
-    elif 91 <= vitorias <= 100:
-        nivel = "Lendário"
-    else:
-        nivel = "Imortal"
+if (vitorias < 10){
+    nivel = "Ferro"
+}    
+else if (vitorias >= 11 && vitorias <= 20){
+    nivel = "Bronze"
+}        
+else if (vitorias >= 21 && vitorias <= 50){
+    nivel = "Prata"
+}   
+else if (vitorias >= 51 && vitorias <= 80){
+    nivel = "Ouro"
+}    
+else if (vitorias >= 81 && vitorias <= 90){
+    nivel = "Diamante"
+}
+else if (vitorias >= 91 && vitorias <= 100){
+    nivel = "Lendário"
+}   
+else{
+    nivel = "Imortal"    
+}
 
-    # Exibe a mensagem com o saldo e o nível
-    mensagem = f"O Herói tem saldo de {saldo_rankeadas} e está no nível de {nivel}"
-    retornar mensagem
+console.log("O Herói tem saldo " +  resultado + " vitórias e está no nível de " + nivel)
 
-# Exemplo de uso da função
-vitorias = 60
-derrotas = 20
-resultado = calcular_nivel(vitorias, derrotas)
-exibir(resultado)
+
+
+
+
+
+   
